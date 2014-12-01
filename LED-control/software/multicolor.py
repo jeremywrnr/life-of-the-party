7b7cd05722eccfa427be0e2daa9d23a5a3169730
Jeremy Warner <jeremy.warner@rochester.edu>
2014-12-01 17:37:58 -0500

    working connect and disconnect scripts
    Former-commit-id: d4a49c54e633347027c6187dae96d605d29cc00d

---
+++ LED-control/software/multicolor.py
@@ -0,0 +1,27 @@
+import sys
+import liblo
+
+node = "192.168.0." + sys.argv[1]
+address = liblo.Address(node,"2222")
+
+#int 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+# rgb colors, from cmd line, range from 0 to 1.
+r = float(sys.argv[2])
+g = float(sys.argv[3])
+b = float(sys.argv[4])
+
+liblo.send(address,'/light/color/set',('f', r),('f', g),('f', b))
+
+
+#int 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+# rgb colors, from cmd line, range from 0 to 1.
+r = float(sys.argv[2])
+g = float(sys.argv[3])
+b = float(sys.argv[4])
+
+liblo.send(address,'/light/color/set',('f', r),('f', g),('f', b))
+
