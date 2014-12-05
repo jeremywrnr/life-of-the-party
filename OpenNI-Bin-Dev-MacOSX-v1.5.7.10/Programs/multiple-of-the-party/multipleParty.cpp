4b30ea3478473e3bfa3f5ebade2b3fc362f4b48b
Justin Fraumeni <justinfraumeni@gmail.com>
2014-12-05 01:31:54 -0500

    Fixed some typos
    Former-commit-id: 218ae2b769e8c443e206944016875ca0718fe079

--- OpenNI-Bin-Dev-MacOSX-v1.5.7.10/Programs/multiple-of-the-party/multipleParty.cpp
+++ OpenNI-Bin-Dev-MacOSX-v1.5.7.10/Programs/multiple-of-the-party/multipleParty.cpp
@@ -275,7 +275,7 @@ int main()
     char command[200];
     bool netpositivev;
     double distancechange;
-    double secondchange0;
+    double secondchange;
     double avg_b, avg_g, avg_r = 0;
 
     double r[3] = {0,0,0};
